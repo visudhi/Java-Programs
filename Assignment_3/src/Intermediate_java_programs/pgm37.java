8f13e9a1c8422d1cd8dfedabbab8ed48cacf22ab
PB Sathyanarayan <pleasantnow@gmail.com>
2023-01-14 08:21:28 +0530

    pgm36 and pgm37 added

---
+++ Assignment_3/src/Intermediate_java_programs/pgm37.java
@@ -0,0 +1,35 @@
+package Intermediate_java_programs;
+
+import java.util.Scanner;
+
+public class pgm37 {
+    public static void main(String[] args) {
+        
+
+        Scanner in = new Scanner(System.in);
+        int nSum = 0, eSum = 0, oSum = 0;
+        System.out.println("Enter Numbers:");
+        while (true) {
+            int n = in.nextInt();
+            
+            if (n == 0) {
+                break;
+            }
+            
+            if (n < 0) {
+                nSum += n; 
+            }
+            else if (n % 2 == 0) {
+                eSum += n;
+            }
+            else {
+                oSum += n;
+            }
+        }
+        in.close();
+        System.out.println("Negative No. Sum = " + nSum);
+        System.out.println("Positive Even No. Sum = " + eSum);
+        System.out.println("Positive Odd No. Sum = " + oSum);
+    }
+}
+    
