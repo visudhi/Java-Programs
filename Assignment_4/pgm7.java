00fdfd410874907188494f2cd603454cdc55ea80
PB Sathyanarayan <pleasantnow@gmail.com>
2023-01-15 17:26:37 +0530

    pgm7 added

---
+++ Assignment_4/pgm7.java
@@ -0,0 +1,44 @@
+//Define a method to find out if a number is prime or not.
+import java.util.Scanner;
+public class pgm7
+{
+
+    public static void main(String[] args)
+    {
+        int x;
+        Scanner sc= new Scanner (System.in);
+        System.out.println("Enter a number");
+        x=sc.nextInt();
+        sc.close();
+        isPrime(x);
+    }
+    
+    static void isPrime(int x)
+    {
+        int i=2,count=0;
+        while(i<x)
+        {
+        
+            if(x%i==0)
+            {
+                count++;
+                break;
+            }
+            i++;
+        }
+        
+
+            if(count==0)
+            {
+                System.out.println("prime");
+            }
+            else
+            {
+            System.out.println("Not prime");
+            }
+
+        
+        
+    }
+
+}
